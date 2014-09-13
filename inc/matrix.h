1bc2423095ac7fa4e50e7e305a5cc63f14a6e426
Gregory Limes <gregory.limes@nasa.gov>
2014-09-13 00:08:07 -0400

    Position Add and Subtract
      - Write the tests
      - Build the APIs
      - Implement the operations
    
    Commit at GREEN status.

--- inc/matrix.h
+++ inc/matrix.h
@@ -24,14 +24,58 @@ namespace Farsyte {
 
     class Position {
       double data[3];
-      public:
-      Position()                             : data{0,0,0} { }
-      Position(double x, double y, double z) : data{x,y,z} { }
+    public:
+
+      Position()
+        : data{0,0,0} {
+      }
+
+      Position(double x, double y, double z)
+        : data{x,y,z} {
+      }
+
       Position(Position const &p)
-        : data{p.data[0], p.data[1], p.data[2]} { }
-      double const & operator()(int i) const { return data[i-1]; }
-      double       & operator()(int i)       { return data[i-1]; }
+        : data{p.data[0], p.data[1], p.data[2]} {
+      }
+
+      double const & operator()(int i) const { 
+        return data[i-1];
+      }
+
+      double       & operator()(int i)       {
+        return data[i-1];
+      }
+
+      Position & operator=(Position const &p) {
+        data[0] = p.data[0];
+        data[1] = p.data[1];
+        data[2] = p.data[2];
+        return *this;
+      }
+
+      Position & operator+=(Position const &p) {
+        data[0] += p.data[0];
+        data[1] += p.data[1];
+        data[2] += p.data[2];
+        return *this;
+      }
+
+      Position & operator-=(Position const &p) {
+        data[0] -= p.data[0];
+        data[1] -= p.data[1];
+        data[2] -= p.data[2];
+        return *this;
+      }
+
     };
+
+    Position operator+(Position L, Position const &R) {
+      return L += R;
+    }
+
+    Position operator-(Position L, Position const &R) {
+      return L -= R;
+    }
 
   }
 }
--- matrix/test/test_matrix.cc
+++ matrix/test/test_matrix.cc
@@ -30,15 +30,23 @@ using std::ostringstream;
 
 #include <iomanip>
 
-static int case_compare(
+int case_compare(
   Test &t, string const &title,
-  double exp,
-  double act)
+  double x, double y, double z,
+  Position const &r)
 {
   t << title << endl
-    << "  expected: " << exp << endl
-    << "  observed: " << act << endl;
-  if (act == exp)
+    << "  expected: "
+    << setw(16) << x
+    << setw(16) << y
+    << setw(16) << z
+    << endl
+    << "  observed: "
+    << setw(16) << r(1)
+    << setw(16) << r(2)
+    << setw(16) << r(3)
+    << endl;
+  if ((x == r(1)) && (y == r(2)) && (z == r(3)))
     return 0;
   t.fail(title + " failed");
   return 1;
@@ -46,42 +54,80 @@ static int case_compare(
 
 int test_matrix_position_ctor(Suite &s) {
 
-  Test t(s, "Position and operator(i,j)");
-
-  int ec = 0;
+  Test t(s, "Constructor (and access)");
 
   Position D;
   Position X {1,0,0};
   Position Y {0,1,0};
   Position Z {0,0,1};
   Position M {4,5,6};
+
+  return 0
+    + case_compare(t, "D initialized", 0, 0, 0, D)
+    + case_compare(t, "X initialized", 1, 0, 0, X)
+    + case_compare(t, "Y initialized", 0, 1, 0, Y)
+    + case_compare(t, "Z initialized", 0, 0, 1, Z)
+    + case_compare(t, "M initialized", 4, 5, 6, M)
+    ;
+}
+
+int test_matrix_position_copy(Suite &s) {
+
+  Test t(s, "Copy Constructor");
+
+  Position A {5,3,7};
+  Position M {4,5,6};
   Position C (M);
 
-  ec += case_compare(t, "X initialized", 0, D(1));
-  ec += case_compare(t, "Y initialized", 0, D(2));
-  ec += case_compare(t, "Z initialized", 0, D(3));
+  M = A;
 
-  ec += case_compare(t, "X.x initialized", 1, X(1));
-  ec += case_compare(t, "X.y initialized", 0, X(2));
-  ec += case_compare(t, "X.z initialized", 0, X(3));
+  return 0
+    + case_compare(t, "C copied from M", 4, 5, 6, C)
+    + case_compare(t, "M copied from A", 5, 3, 7, M)
+    ;
 
-  ec += case_compare(t, "Y.x initialized", 0, Y(1));
-  ec += case_compare(t, "Y.y initialized", 1, Y(2));
-  ec += case_compare(t, "Y.z initialized", 0, Y(3));
+}
 
-  ec += case_compare(t, "Z.x initialized", 0, Z(1));
-  ec += case_compare(t, "Z.y initialized", 0, Z(2));
-  ec += case_compare(t, "Z.z initialized", 1, Z(3));
+int test_matrix_position_add(Suite &s) {
 
-  ec += case_compare(t, "M.x initialized", 4, M(1));
-  ec += case_compare(t, "M.y initialized", 5, M(2));
-  ec += case_compare(t, "M.z initialized", 6, M(3));
+  Test t(s, "Position Addition");
 
-  ec += case_compare(t, "C.x initialized", 4, C(1));
-  ec += case_compare(t, "C.y initialized", 5, C(2));
-  ec += case_compare(t, "C.z initialized", 6, C(3));
+  Position A {3,2,1};
+  Position B {3,5,7};
 
-  return ec;
+  Position S, R;
+
+  S = (A +  B);
+  R = (A += B);
+
+  return 0
+    + case_compare(t, "S gets Sum", 6, 7, 8, S)
+    + case_compare(t, "A incremented by B", 6, 7, 8, A)
+    + case_compare(t, "B not modified", 3, 5, 7, B)
+    + case_compare(t, "R also gets A", 6, 7, 8, R)
+    ;
+
+}
+
+int test_matrix_position_sub(Suite &s) {
+
+  Test t(s, "Position Difference");
+
+  Position A {4,6,8};
+  Position B {3,2,1};
+
+  Position D, R;
+
+  D = (A -  B);
+  R = (A -= B);
+
+  return 0
+    + case_compare(t, "D gets Difference", 1, 4, 7, D)
+    + case_compare(t, "A incremented by B", 1, 4, 7, A)
+    + case_compare(t, "B not modified", 3, 2, 1, B)
+    + case_compare(t, "R also gets A", 1, 4, 7, R)
+    ;
+
 }
 
 int test_matrix_position(Log &log) {
@@ -100,6 +146,9 @@ int test_matrix_position(Log &log) {
 
   return 0
     + test_matrix_position_ctor(s)
+    + test_matrix_position_copy(s)
+    + test_matrix_position_add(s)
+    + test_matrix_position_sub(s)
     ;
 
 }
